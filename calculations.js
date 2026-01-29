93bdaab5e3be2bb90a303af24178d042fb14329a
havelockwool <calvin@havelockwool.com>
2026-01-29 13:14:22 -0800

    update production metrics display to include additional line break for better readability

--- calculations.js
+++ calculations.js
@@ -244,7 +244,8 @@ function updateWarehouseAnalysis() {
         title: {
             text: `<b>Production Metrics Comparison with Varying Production Hours</b><br>
             16OC Ratio: ${PRODUCT_DIST_PERCENT.toFixed(0)}%<br>
-            Production Speed Factor: ${PRODUCTION_REDUCE_SPEED_FACTOR.toFixed(2)}<br>`,
+            Production Speed Factor: ${PRODUCTION_REDUCE_SPEED_FACTOR.toFixed(2)}<br>
+            <br>`,
 
             font: {
                 size: 16
